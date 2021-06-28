e6ba73b3637904dce2c2a8c024e11e8558d657da
madgenius021 <akiosatori021@gmail.com>
2021-06-28 12:01:23 +0500

    chinese language added

--- src/languages/i18n.js
+++ src/languages/i18n.js
@@ -3,6 +3,7 @@ import English from './English';
 import German from './German';
 import Japanese from './Japanese';
 import Spanish from './Spanish';
+import Chinese from './Chinese';
 import {initReactI18next} from 'react-i18next';
 
 i18next.use(initReactI18next).init({
@@ -13,6 +14,7 @@ i18next.use(initReactI18next).init({
     de: German,
     ja: Japanese,
     es: Spanish,
+    zh: Chinese,
   },
   react: {
     useSuspense: true,
