4bd12002de018a97585c884de2169acae34344c2
dev_ninja <starhelios94@gmail.com>
2021-08-13 07:26:18 -0400

    rectangle / box optimization for the smallest square

--- src/functions/annotation.js
+++ src/functions/annotation.js
@@ -7,6 +7,9 @@ import {
   } from '../services/API/APIManager';
 
  import i18n from '../languages/i18n';
+import { block } from 'react-native-reanimated';
+
+import groupBy from 'lodash.groupby'
 
  export const initial_bounties = [
     {tag: 'anonymization bounty', desc: 'Anonymization Bounty (photos of faces)', checked: false, disabled: false},
@@ -147,10 +150,12 @@ import {
     const image_id = props.curMetadata.image_id;
     const originalImageWidth = props.zoomView.props.imageWidth;
     const originalImageHeight = props.zoomView.props.imageHeight;
+    const optBlocks = optimizeBlocks(props.annoRect);
 
     var _rects = [];
-    props.annoRect.map((value)=>{
-      //should change the scale issue
+    //props.annoRect.map((value)=>{
+    optBlocks.map((value)=>{  
+    //should change the scale issue
       _rects.push({type: 'box', 
         tag: value.tag, 
         x: (value.x / originalImageWidth), 
@@ -310,4 +315,109 @@ import {
       _tags.push(value);
     });
     props.setAnnotationTags(_tags);
+  };
+
+  const isOverlaps = (x,y, width, height, x2, y2, width2, height2) => {
+    if(( x < x2 + width2) &&
+      (y < y2 + height2) &&
+      (x2 < x + width) &&
+      (y2 < y + height)) 
+      return true;
+    return false;
   }
+
+  const get_box_size = (curblock, blocks)=>{
+    const {x, y, width, height} = curblock;
+    //filterout same blocks
+    const t_blocks = blocks.filter((block)=>block.width == width && block.height == height)
+
+    //find width
+    var max_width = 1
+    while(true){
+      const found = t_blocks.find((block)=>{
+        return isOverlaps(x+max_width*width, y, width, height,
+          block.x, block.y, block.width, block.height
+          )
+      })
+
+      if(found) {max_width++;}
+      else break;
+    }
+    
+    //find max_height
+    var max_height = 1
+    var stop = false
+    while(true){
+
+      for(var i = 0; i < max_width; i++) {
+        const found = t_blocks.find((block)=>isOverlaps(
+          x + i*width, y + max_height * height, width, height,
+          block.x, block.y, block.width, block.height
+        ))
+
+        if (!found){
+          stop = true;
+          break;
+        } 
+      }
+
+      if(!stop){
+        max_height++;
+      }
+      else break;
+    }
+    //find height
+    return {max_width, max_height}
+  };
+
+  export const optimizeBlocks = (blocks)=>{
+    //group by tag
+    var optBlocks = [];
+    const tag_group = groupBy(blocks, (block)=>(block.tag));
+    const tag_keys = Object.keys(tag_group);
+
+    tag_keys.map((tag_key)=>{
+      var _blocks = [];
+          //group by width| hieght
+      const group = groupBy(tag_group[tag_key], (block)=>(block.width));
+      //retrieve rect size
+      const group_keys = Object.keys(group);
+      
+      group_keys.map((key)=>{
+        const grouped_blocks = group[key];
+        var blocks_per_width = [];
+        grouped_blocks.map((block)=>{
+          const {max_width, max_height} = get_box_size(block, grouped_blocks);
+          //add lists
+          blocks_per_width.push({
+            x: block.x,
+            y: block.y,
+            type: 'box',
+            tag: block.tag,
+            width: max_width * block.width, 
+            height: max_height * block.height});
+        });
+        //sort rect by area size
+        blocks_per_width.sort((b1, b2)=>(b2.width * b2.height - b1.width * b1.height));
+        if(blocks_per_width.length > 0){
+          _blocks.push(blocks_per_width[0]);
+        }
+        //filter out overlaps
+        blocks_per_width.map((block, index)=>{
+
+          const found = _blocks.find((t_block, t_index)=>{
+            //skip self compare
+            return isOverlaps(block.x, block.y, block.width, block.height, t_block.x, t_block.y, t_block.width, t_block.height)
+          })
+
+          if(!found){
+            //final block
+            _blocks.push(block);
+          }
+        });
+        optBlocks = [..._blocks];
+      });
+
+    });
+    return optBlocks;
+  };
