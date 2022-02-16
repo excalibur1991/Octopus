1c5ea2c5e5a1598626f78c611641ef9d377bf0f3
dev_ninja <starhelios94@gmail.com>
2022-02-16 14:00:30 -0500

    T&C bottom gradient and TCModal InfoModal pulled out from screens

--- src/screens/ImageVerifyMission.js
+++ src/screens/ImageVerifyMission.js
@@ -10,101 +10,8 @@ const VerifyMission = require('../assets/image_verify_mission_test.png');
 const CompanyIcon = require('../assets/company_icon_verify.png');
 import {actions} from '../services/State/Reducer';
 import {useStateValue} from '../services/State/State';
-
-const InfoModal = ({open = false, onClose = () => {}}) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  return (
-    <Modal transparent visible={open} statusBarTranslucent animationType="fade">
-      <View style={styles.infoModalContainer}>
-        <View style={styles.infoModalContentContainer}>
-          <View style={styles.header}>
-            <FontAwesome5Icon
-              size={42}
-              name="list-alt"
-              color={theme.COLORS.WHITE}
-              style={styles.buttonIcon}
-            />
-            <Text style={styles.headerTitle}>About Mission</Text>
-          </View>
-          <ScrollView
-            horizontal
-            pagingEnabled
-            nestedScrollEnabled
-            onScroll={(e) => {
-              const index = Math.round(
-                (e.nativeEvent.contentOffset.x /
-                  Dimensions.get('screen').width) *
-                  0.9,
-              );
-              setCurrentIndex(index);
-            }}
-            showsHorizontalScrollIndicator={false}>
-            <View style={styles.infoContainerItems}>
-              <View style={styles.infoContainerItem}>
-                <Text style={styles.infoContentItemTitle}>Title1</Text>
-                <Text style={styles.infoContentItemDescription}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aenean et tellus justo. Sed nec sodales est. Nunc venenatis
-                  tellus at leo posuere, vitae interdum mi consequat.
-                  Pellentesque nec lacus
-                </Text>
-              </View>
-              <View style={styles.infoContainerItem}>
-                <Text style={styles.infoContentItemTitle}>Rewards</Text>
-                <Text style={styles.infoContentItemDescription}>
-                  lacinia, congue sapien quis, dapibus Duis et molestie
-                  ligula,nec maximus erat. Donec dapibus, justo sed viverra
-                  sagittis, eros sapien consequat nunc, Duis et molestie
-                  ligula,nec maximus erat. Donec dapibus, justo sed viverra.
-                </Text>
-              </View>
-            </View>
-            <View style={styles.infoContainerItems}>
-              <View style={styles.infoContainerItem}>
-                <Text style={styles.infoContentItemTitle}>Requirements</Text>
-                <Text style={styles.infoContentItemDescription}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aenean et tellus justo. Sed nec sodales est. Nunc venenatis
-                  tellus at leo posuere, vitae interdum mi consequat.
-                  Pellentesque nec lacus
-                </Text>
-              </View>
-              <View style={styles.infoContainerItem}>
-                <Text style={styles.infoContentItemTitle}>Rewards</Text>
-                <Text style={styles.infoContentItemDescription}>
-                  lacinia, congue sapien quis, dapibus Duis et molestie
-                  ligula,nec maximus erat. Donec dapibus, justo sed viverra
-                  sagittis, eros sapien consequat nunc, Duis et molestie
-                  ligula,nec maximus erat. Donec dapibus, justo sed viverra.
-                </Text>
-              </View>
-            </View>
-          </ScrollView>
-          <View style={styles.bottomContainer}>
-            <View style={styles.dots}>
-              {Array.from(Array(2).keys()).map((_, index) => (
-                <Ripple
-                  key={index}
-                  style={currentIndex === index ? styles.dot : styles.dotActive}
-                />
-              ))}
-            </View>
-            <LinearGradient
-              end={{x: 1, y: 0}}
-              start={{x: 0.15, y: 0}}
-              colors={[theme.COLORS.LIGHT_PURPLE, theme.COLORS.LIGHT_BLUE]}
-              style={styles.modalButtonGradient}>
-              <Ripple onPress={onClose} style={styles.modalButton}>
-                <Text style={styles.buttonText}>Got It</Text>
-              </Ripple>
-            </LinearGradient>
-          </View>
-        </View>
-      </View>
-    </Modal>
-  );
-};
+import { TCModal } from '../components/TCModal';
+import { AboutMissionModal } from '../components/AboutMissionModal';
 
 const RadioButton = ({checked, onCheckChange}) => {
   return (
@@ -121,64 +28,6 @@ const RadioButton = ({checked, onCheckChange}) => {
   );
 };
 
-const TCModal = ({open = false, onClose = () => {}}) => {
-  return (
-    <Modal
-      transparent
-      visible={open}
-      statusBarTranslucent
-      animationType="slide">
-      <View style={styles.tcModalContainer}>
-        <View style={styles.tcModalContentContainer}>
-          <Text style={styles.tcHeaderTitle}>Terms &amp; Conditions</Text>
-          <View style={styles.mainDivider} />
-          <ScrollView
-            showsVerticalScrollIndicator={false}
-            contentContainerStyle={styles.tcContainerItems}>
-            <View style={styles.tcContainerItem}>
-              <Text style={styles.tcContentItemTitle}>
-                Vivamus pulvinar neque
-              </Text>
-              <Text style={styles.tcContentItemDescription}>
-                Lorem ipsum dolor sit amet, consectetu r adipiscing elit. Aenean
-                et tellus justo. Sed nec sodales est. Nunc venenatis tellus at
-                leo posuere, vitae interdum mi consequat. Pellentesque nec lacus
-                Lorem ipsum dolor sit amet, consectetu r adipiscing elit. Aenean
-                et tellus justo. Sed nec sodales est. Nunc venenatis tellus at
-                leo posuere, vitae interdum mi consequat. Pellentesque nec lacus
-                Lorem ipsum dolor sit amet, consectetu r adipiscing elit. Aenean
-                et tellus justo. Sed nec sodales est. Nunc venenatis tellus at
-                leo posuere, vitae interdum mi consequat. Pellentesque nec lacus
-              </Text>
-            </View>
-            <View style={styles.tcContainerItem}>
-              <Text style={styles.tcContentItemTitle}>
-                Vestibulum ante ipsum
-              </Text>
-              <Text style={styles.tcContentItemDescription}>
-                lacinia, congue sapien quis, dapibus Duis et molestie ligula,nec
-                maximus erat. Donec dapibus, justo sed viverra sagittis, eros
-                sapien consequat nunc, Duis et molestie ligula,nec maximus erat.
-                Donec dapibus, justo sed viverra . Lorem ipsum dolor sit amet,
-                consectetu r adipiscing elit. Aenean et tellus justo. Sed nec
-                sodales est. Nunc venenatis tellus at leo posuere, vitae
-                interdum mi consequat. Pellentesque nec lacus
-              </Text>
-            </View>
-          </ScrollView>
-          <LinearGradient
-            colors={['transparent', theme.COLORS.BLACK]}
-            style={styles.tcCloseButtonContainer}>
-            <Ripple onPress={onClose} style={styles.tcCloseButton}>
-              <AntIcon size={23} name="close" color={theme.COLORS.WHITE} />
-            </Ripple>
-          </LinearGradient>
-        </View>
-      </View>
-    </Modal>
-  );
-};
-
 const ImageVerifyMission = ({navigation}) => {
   const [showInfo, setShowInfo] = useState(false);
   const [showTCButton, setShowTCButton] = useState(false);
@@ -188,7 +37,7 @@ const ImageVerifyMission = ({navigation}) => {
 
   return (
     <>
-      <InfoModal open={showInfo} onClose={() => setShowInfo(false)} />
+      <AboutMissionModal open={showInfo} onClose={() => setShowInfo(false)} />
       <TCModal open={showTC} onClose={() => setShowTC(false)} />
       <View style={styles.container}>
         <Image
--- src/screens/Information.js
+++ src/screens/Information.js
@@ -14,6 +14,7 @@ import LinearGradient from 'react-native-linear-gradient';
 import {theme} from '../services/Common/theme';
 import {useStateValue} from '../services/State/State';
 import {actions} from '../services/State/Reducer';
+import { AboutMissionModal } from '../components/AboutMissionModal';
 
 const buttons = [
   {title: 'What is Data Union?', action: () => {}},
@@ -29,100 +30,6 @@ const buttons = [
   },
 ];
 
-const InfoModal = ({open = false, onClose = () => {}}) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  return (
-    <Modal transparent visible={open} statusBarTranslucent animationType="fade">
-      <View style={styles.infoModalContainer}>
-        <View style={styles.infoModalContentContainer}>
-          <View style={styles.header}>
-            <Text style={styles.headerTitle}>What is Data Union?</Text>
-          </View>
-          <ScrollView
-            horizontal
-            pagingEnabled
-            nestedScrollEnabled
-            onScroll={(e) => {
-              const index = Math.round(
-                (e.nativeEvent.contentOffset.x /
-                  Dimensions.get('screen').width) *
-                  0.9,
-              );
-              setCurrentIndex(index);
-            }}
-            showsHorizontalScrollIndicator={false}>
-            <View style={styles.infoContainerItems}>
-              <View style={styles.infoContainerItem}>
-                <Text style={styles.infoContentItemTitle}>Title1</Text>
-                <Text style={styles.infoContentItemDescription}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aenean et tellus justo. Sed nec sodales est. Nunc venenatis
-                  tellus at leo posuere, vitae interdum mi consequat.
-                  Pellentesque nec lacus
-                </Text>
-              </View>
-              <View style={styles.infoContainerItem}>
-                <Text style={styles.infoContentItemTitle}>Rewards</Text>
-                <Text style={styles.infoContentItemDescription}>
-                  lacinia, congue sapien quis, dapibus Duis et molestie
-                  ligula,nec maximus erat. Donec dapibus, justo sed viverra
-                  sagittis, eros sapien consequat nunc, Duis et molestie
-                  ligula,nec maximus erat. Donec dapibus, justo sed viverra.
-                </Text>
-              </View>
-            </View>
-            <View style={styles.infoContainerItems}>
-              <View style={styles.infoContainerItem}>
-                <Text style={styles.infoContentItemTitle}>Title1</Text>
-                <Text style={styles.infoContentItemDescription}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aenean et tellus justo. Sed nec sodales est. Nunc venenatis
-                  tellus at leo posuere, vitae interdum mi consequat.
-                  Pellentesque nec lacus
-                </Text>
-              </View>
-              <View style={styles.infoContainerItem}>
-                <Text style={styles.infoContentItemTitle}>Rewards</Text>
-                <Text style={styles.infoContentItemDescription}>
-                  lacinia, congue sapien quis, dapibus Duis et molestie
-                  ligula,nec maximus erat. Donec dapibus, justo sed viverra
-                  sagittis, eros sapien consequat nunc, Duis et molestie
-                  ligula,nec maximus erat. Donec dapibus, justo sed viverra.
-                </Text>
-              </View>
-            </View>
-          </ScrollView>
-          <View style={styles.bottomContainer}>
-            <View style={styles.dots}>
-              {Array.from(Array(2).keys()).map((_, index) => (
-                <Ripple
-                  key={index}
-                  style={currentIndex === index ? styles.dot : styles.dotActive}
-                />
-              ))}
-            </View>
-            <LinearGradient
-              end={{x: 1, y: 0}}
-              start={{x: 0.15, y: 0}}
-              colors={[theme.COLORS.LIGHT_PURPLE, theme.COLORS.LIGHT_BLUE]}
-              style={styles.buttonOuter}>
-              <Ripple onPress={onClose} style={styles.buttonInner}>
-                <Text
-                  style={{
-                    ...styles.itemTitle,
-                    color: theme.COLORS.WHITE,
-                  }}>
-                  Got It
-                </Text>
-              </Ripple>
-            </LinearGradient>
-          </View>
-        </View>
-      </View>
-    </Modal>
-  );
-};
 
 const Information = ({navigation}) => {
   const [open, setOpen] = useState(false);
@@ -130,7 +37,7 @@ const Information = ({navigation}) => {
 
   return (
     <>
-      <InfoModal open={open} onClose={() => setOpen(false)} />
+      <AboutMissionModal open={open} onClose={() => setOpen(false)} />
       <FlatList
         data={buttons}
         style={styles.container}
